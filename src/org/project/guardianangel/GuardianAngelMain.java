a9b22c96f389798d8e0409d50ed5ed4e636a6273
Jackie Jin <liveaeon@gmail.com>
2014-02-14 19:54:03 -0800

    project up

---
+++ src/org/project/guardianangel/GuardianAngelMain.java
@@ -0,0 +1,23 @@
+package org.project.guardianangel;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class GuardianAngelMain extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guardian_angel_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.guardian_angel_main, menu);
+        return true;
+    }
+    
+}
